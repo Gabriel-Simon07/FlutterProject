a99ecb455a096ac994e8ace4227d62ddf64d8231
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-08 19:18:06 -0300

    if, else and null safe

--- bin/initial_project.dart
+++ bin/initial_project.dart
@@ -1,5 +1,21 @@
 void main(List<String> arguments) {
+  final String nome = 'gabriel';
+  bool maiorIdade;
+  bool? geek;
   int idade = 22;
-  double altura = 780e6;
-  print(altura);
+  List<String> names = ['gabriel', 'joao', 'maria'];
+  List<dynamic> gabriel = [22, 1.86, true, 'Gabriel'];
+  print(names);
+  print(
+      'Eu sou ${gabriel[3]}, tenho ${gabriel[1]} de altura e tenho ${gabriel[0]} anos');
+
+  if(geek == true) {
+
+  }
+
+  if (idade >= 18) {
+    maiorIdade = true;
+  } else {
+    maiorIdade = false;
+  }
 }
