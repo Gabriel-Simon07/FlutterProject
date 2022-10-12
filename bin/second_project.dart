d207d38b1dada90a0f7b3885d2894ed7f0f67a62
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-11 22:20:12 -0300

    type of returns and required and optional parameters in functions

--- bin/second_project.dart
+++ bin/second_project.dart
@@ -7,6 +7,22 @@ void main() {
   bool isMadura;
 
   frutaEstaMadura(diasDesdeColheita);
+  // mostrarMadura(nome, diasDesdeColheita, cor: 'laranja');
+  mostrarMadura(nome, diasDesdeColheita, cor: 'Roxa');
+  int valor = quantoTempoFalta(diasDesdeColheita);
+  print('Faltam $valor dias');
+}
+// void mostrarMadura(String nome, int dias, {String cor = 'sem cor'}) {
+void mostrarMadura(String nome, int dias, {required String cor}) {
+  if(dias >= 30) {
+    print('A $nome está madura');
+  } else {
+    print('A $nome não está madura');
+  }
+
+  if(cor != null) {
+    print('A $nome é $cor');
+  }
 }
 
 bool frutaEstaMadura(int diasDesdeColheita) {
@@ -15,4 +31,9 @@ bool frutaEstaMadura(int diasDesdeColheita) {
   } else {
     return false;
   }
+}
+
+int quantoTempoFalta(int diasDesdeColheita) {
+  int diasParaMadura = 30;
+  return diasDesdeColheita - diasParaMadura;
 }
