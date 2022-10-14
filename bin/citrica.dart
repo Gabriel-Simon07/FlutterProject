7a4e101c8f4682f2ca21832e7bb6788e5c738159
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-13 21:48:30 -0300

    implemented class

---
+++ bin/citrica.dart
@@ -0,0 +1,10 @@
+class Citricas {
+  String nome;
+  double peso;
+  String cor;
+  int diasDesdeColheita;
+  bool? isMadura;
+  double nivelAzedo;
+
+  Citricas(this.nome, this.peso, this.cor, this.diasDesdeColheita, this.isMadura, this.nivelAzedo);
+}
---
+++ bin/nozes.dart
@@ -0,0 +1,10 @@
+class Nozes {
+  String nome;
+  double peso;
+  String cor;
+  int diasDesdeColheita;
+  bool? isMadura;
+  double porcentagemOleoNatural;
+
+  Nozes(this.nome, this.peso, this.cor, this.diasDesdeColheita, this.isMadura, this.porcentagemOleoNatural);
+}
