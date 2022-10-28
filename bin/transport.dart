805d80b11971d5ce876827228babea2a4db00829
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-27 22:03:25 -0300

    switch

--- bin/transport.dart
+++ bin/transport.dart
@@ -1,14 +1,28 @@
 void main() {
-  escolheMeioTransporte(Transporte.BIKE);
+  escolheMeioTransporteSemSwitch(Transporte.BIKE);
+  escolheMeioTransporteComSwitch(Transporte.AVIAO);
 }
 
-void escolheMeioTransporte(Transporte transporte){
+void escolheMeioTransporteSemSwitch(Transporte transporte) {
   if(transporte == Transporte.CARRO) {
     print('Vou de carro');
   } else if(transporte == Transporte.ONIBUS) {
     print('Vou de ônibus');
   } else {
     print('Vou de outro tipo de transporte');
+  }
+}
+
+void escolheMeioTransporteComSwitch(Transporte transporte) {
+  switch(transporte) {
+    case Transporte.CARRO:
+      print('Vou de carro');
+    break;
+    case Transporte.ONIBUS:
+      print('Vou de ônibus');
+    break;
+    default:
+      print('Vou de outro tipo de transporte');
   }
 }
 
