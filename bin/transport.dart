6fe24f59f2a73fc86469b446fd085dd430a89089
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-29 17:25:44 -0300

    add Set of String

--- bin/transport.dart
+++ bin/transport.dart
@@ -1,11 +1,20 @@
 import 'enum/Transporte.dart';
 
 void main() {
+  Set<String> registrosVisitados = <String>{};
   escolheMeioTransporte(Transporte.AVIAO);
+  print(registrarDestinos('teste' ,registrosVisitados));
+  print(registrarDestinos('teste' ,registrosVisitados));
+}
+
+String registrarDestinos(String destino, Set<String> registrosVisitados) {
+  registrosVisitados.add(destino);  
+  return destino;
 }
 
 void escolheMeioTransporte(Transporte transporte) {
-  switch(transporte) {
+  switch
+  (transporte) {
     case Transporte.CARRO:
       print('Vou de carro');
     break;
