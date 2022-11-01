c46683426ec84bfd4ff3d1279c43a89e6631e325
Gabriel Simon <gabrielsimon775@gmail.com>
2022-10-31 21:41:56 -0300

    Created classes Account and ControllerAccount

---
+++ bin/project3/controllers/account_controller.dart
@@ -0,0 +1,43 @@
+import '../models/account.dart';
+
+class AccountController {
+  final Map<String, Account> _database = {};
+
+  addCount({required String id, required Account account}) {
+    _database[id] = account;
+  }
+
+  bool makeTransfer({required String idSender, required String idReceiver, required double amount}) {
+    if(!verifyId(idSender)) {
+      return false;
+    }
+
+    if(!verifyId(idReceiver)) {
+      return false;
+    }
+    
+    if(!verifyId(idReceiver)) {
+      return false;
+    }
+
+    Account accountSender = _database[idSender]!;
+    Account accountReceiver = _database[idReceiver]!;
+
+   if (!accountSender.isAuthenticated) {
+     return false;
+   }
+
+   if (accountSender.balance < amount) {
+     return false;
+   }
+
+   accountSender.balance -= amount;
+   accountReceiver.balance += amount;
+
+    return true;
+  }
+
+  bool verifyId(String id) {
+    return _database.containsKey(id);
+  }
+}
